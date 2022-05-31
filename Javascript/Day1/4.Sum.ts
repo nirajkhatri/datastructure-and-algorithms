84b8b788c69ac456fa6fcb1699133e6b6daa48de
Niraj Khatri <nirajkhatri.official@gmail.com>
2022-05-31 16:23:45 +0545

    Planning project structure

--- Javascript/Day1/4.Sum.ts
+++ Javascript/Day1/4.Sum.ts
@@ -6,20 +6,3 @@ function sum(num1: number, num2: number) {
 }
 //log the sum value
 console.log(sum(2, 4));
-
-//Problem solving approach
-
-//1.can i restate the problem in my own words?
-//"implement addition"
-
-//2.What are the inputs that go into the problem?
-//ints?floats?string?
-
-//3. What are the output that should come from the solution to the problem?
-//ints?floats?string?
-
-//4.Can the outputs be determined from the inputs?
-//null?undefined?0?
-
-//5.How should I label the important pieces of data that are a part of the problem?
-//
